bb9b105d816efcdf4c2495aee02813f829f430b3
Rodrigo V. Cortezi <rodrigovcortezi@gmail.com>
2023-09-21 21:12:58 -0300

    refactor(repository): fix interface return types

--- src/repository/user.ts
+++ src/repository/user.ts
@@ -2,7 +2,7 @@ import type {CreateUserDto, UpdateUserDto} from '~/dtos/user'
 import {User} from '~/models/user'
 
 export interface UserRepository {
-  create: (user: CreateUserDto) => Promise<User | undefined>
-  update: (id: number, user: UpdateUserDto) => Promise<User | undefined>
-  findByEmail: (email: string) => Promise<User | null | undefined>
+  create: (user: CreateUserDto) => Promise<User>
+  update: (id: number, user: UpdateUserDto) => Promise<User>
+  findByEmail: (email: string) => Promise<User | null>
 }
