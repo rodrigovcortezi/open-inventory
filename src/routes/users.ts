59fe8bdd75713c428579e31e6232861bc73376fe
Rodrigo V. Cortezi <rodrigovcortezi@gmail.com>
2023-09-21 21:32:32 -0300

    fix(validation): use correct schema validation

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,11 +1,11 @@
 import Router from 'koa-router'
 import {createUser, loginUser, updateUser} from '~/controllers/users'
 import {validate} from '~/middlewares/validate'
-import {UserSchema, PartialUserSchema, LoginUserSchema} from '~/dtos/user'
+import {CreateUserSchema, LoginUserSchema, PartialUserSchema} from '~/dtos/user'
 
 const router = new Router({prefix: '/users'})
 
-router.post('/new', validate(UserSchema), createUser)
+router.post('/new', validate(CreateUserSchema), createUser)
 router.put('/:id', validate(PartialUserSchema), updateUser)
 router.post('/login', validate(LoginUserSchema), loginUser)
 
